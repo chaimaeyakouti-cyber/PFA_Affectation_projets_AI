911cb4079ba758b5da2d645426dafa2725f2d422
shymoon <chaimaeyakouti@gmail.com>
2026-05-15 22:06:21 +0100

    initiation du frontend
    installation des dépendances nécessaires et construction de l architecture du frontend et lancement du serveur

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'http://127.0.0.1:8000'
+})
+
+// GROUPES
+export const creerGroupe = (data: any) => API.post('/groupes/', data)
+export const getGroupes = () => API.get('/groupes/')
+
+// ENCADRANTS
+export const creerEncadrant = (data: any) => API.post('/encadrants/', data)
+export const getEncadrants = () => API.get('/encadrants/')
+
+// PROJETS
+export const creerProjet = (data: any) => API.post('/projets/', data)
+export const getProjets = () => API.get('/projets/')
+
+// CHOIX
+export const creerChoix = (data: any) => API.post('/choix/', data)
+export const getChoix = () => API.get('/choix/')
+
+// AFFECTATION
+export const lancerAffectation = () => API.post('/affecter/')
+export const getAffectations = () => API.get('/affectations/')
+export const validerAffectation = (id: number) => API.put(`/affectations/${id}/valider`)
+export const modifierAffectation = (id: number, nouveau_projet_id: number) => 
+  API.put(`/affectations/${id}/modifier?nouveau_projet_id=${nouveau_projet_id}`)
